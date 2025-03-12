2e62f1362ccddfdad7b2b0f7905771207059a6d5
Berat Sakipi <berat.sakipi@gmail.com>
2025-03-12 22:29:05 +0100

    Created the contents for the header (the logo and the profile picture)

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <header>
+            <a href="/" className="logo"><span>Bésà</span></a>
+            <span className="profile-img-container-span">
+                by
+                <a href="https://github.com/justberatt" target="_blank" className="profile-img-container">
+                    <img src="https://avatars.githubusercontent.com/u/70145882?u=97a1b874511dec217f17b84db14531c7a9e30022&v=4" />
+                </a>
+            </span>
+        </header>
+    )
+}
+
+export default Header
